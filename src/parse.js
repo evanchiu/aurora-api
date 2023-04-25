c88e37743d323c26caa78668eeccb4e9e330ded0
Aline Fernanda Bellozo <alinebellozo@gmail.com>
2023-04-24 17:29:15 -0700

    Alter regex to make it compatible with API
    - Sort the API output
    
    - Kp index is now published with 2 decimal numbers in NOAA's report,
      REGEX was changed to match it. Returned Kp is still integer (JSON
      string) to not break the API;
    
    - Regex changed to ignore the geomagnetic scale, which comes after the
      Kp index inside parenthesis, for example: (G3) (link to the
      geomagnetic scale: https://www.swpc.noaa.gov/noaa-scales-explanation)
    
    NOAA's report example:
    
    :Product: 3-Day Forecast
    :Issued: 2023 Apr 24 1230 UTC
    A. NOAA Geomagnetic Activity Observation and Forecast
    
    The greatest observed 3 hr Kp over the past 24 hours was 8 (NOAA Scale
    G4).
    The greatest expected 3 hr Kp for Apr 24-Apr 26 2023 is 8.00 (NOAA Scale
    G4).
    
    NOAA Kp index breakdown Apr 24-Apr 26 2023
    
                 Apr 24       Apr 25       Apr 26
    00-03UT       7.00 (G3)    5.33 (G1)    2.33
    03-06UT       8.00 (G4)    4.33         2.67
    06-09UT       6.67 (G3)    4.00         2.33
    09-12UT       6.33 (G2)    3.00         2.00
    12-15UT       5.67 (G2)    3.00         1.33
    15-18UT       4.67 (G1)    2.33         1.00
    18-21UT       3.00         2.00         1.33
    21-00UT       4.00         2.67         2.33
    
    Rationale: The geomagnetic field is expected to be at G1-G2
    (Minor-Moderate) levels through the rest of 24 Apr with G1 (Minor)
    levels likely on 25 Apr as CME effects slowly diminish.
    
    B. NOAA Solar Radiation Activity Observation and Forecast
    
    Solar radiation, as observed by NOAA GOES-16 over the past 24 hours, was
    above S-scale storm level thresholds.
    
    Solar Radiation Storm Forecast for Apr 24-Apr 26 2023
    
                  Apr 24  Apr 25  Apr 26
    S1 or greater    1%      1%      1%
    
    Rationale: No S1 (Minor) or greater solar radiation storms are expected.
    No significant active region activity favorable for radiation storm
    production is forecast.
    
    C. NOAA Radio Blackout Activity and Forecast
    
    No radio blackouts were observed over the past 24 hours.
    
    Radio Blackout Forecast for Apr 24-Apr 26 2023
    
                  Apr 24        Apr 25        Apr 26
    R1-R2           25%           25%           10%
    R3 or greater    1%            1%            1%
    
    Rationale: There is a chance for R1-R2 (Minor-Moderate) radio blackouts
    on 24-25 Apr.
    
    Sort the API output
    
    Just sort by date to easy reading.
    
    Signed-off-by: Aline Fernanda Bellozo <alinebellozo@gmail.com>

--- src/parse.js
+++ src/parse.js
@@ -12,32 +12,44 @@ module.exports = function(text) {
   if (!datesMatch) { throw new Error('Failed to parse issued dates'); }
 
   // Capture the data rows
-  let dataMatch = text.match(/(\d{2})-(\d{2})UT\s+(\d)\s+(\d)\s+(\d)/g);
+  let dataMatch = text.match(/(\d{2})-(\d{2})UT\s+(\d.\d{2}).*(\d.\d{2}).*(\d.\d{2})/g);
   if (!dataMatch || !dataMatch.length == 8) { throw new Error('Failed to find data'); }
 
   // Parse the data rows
   let breakdown = [];
   for (let i = 0; i < dataMatch.length; i++) {
-    let lineMatch = dataMatch[i].match(/(\d{2})-(\d{2})UT\s+(\d)\s+(\d)\s+(\d)/);
+    let lineMatch = dataMatch[i].match(/(\d{2})-(\d{2})UT\s+(\d.\d{2}).*(\d.\d{2}).*(\d.\d{2})/);
     if (!lineMatch) { throw new Error('Failed to parse line'); }
 
     // Store entry for each column
     breakdown.push({
       start: new Date([year, datesMatch[1], datesMatch[2], lineMatch[1] + ':00', 'UTC'].join(' ')),
-      kp: lineMatch[3]
+      kp: Math.round(lineMatch[3]).toString()
     });
     breakdown.push({
       start: new Date([year, datesMatch[3], datesMatch[4], lineMatch[1] + ':00', 'UTC'].join(' ')),
-      kp: lineMatch[4]
+      kp: Math.round(lineMatch[4]).toString()
     });
     breakdown.push({
-      start: new Date([year, datesMatch[5], datesMatch[6], lineMatch[1] + ':00', 'UTC'].join(' ')),
-      kp: lineMatch[5]
+      start: new Date([year, datesMatch[5], datesMatch[6],lineMatch[1] + ':00', 'UTC'].join(' ')),
+      kp: Math.round(lineMatch[5]).toString()
     });
   }
-
+  breakdown = breakdown.sort(GetSortOrder());
   return {
     issued,
     breakdown
   }
 }
+
+//Comparer Function    
+function GetSortOrder(prop = 'start') {
+  return function (a, b) {
+    if (a[prop] > b[prop]) {
+      return 1;
+    } else if (a[prop] < b[prop]) {
+      return -1;
+    }
+    return 0;
+  };
+}    
